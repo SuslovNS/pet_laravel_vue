76938ac0706db9a58ffe414cee7e9f758adc079a
SuslovNS <nikitka536@mail.ru>
2022-06-02 21:44:46 +0500

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Laravel+Vue Table</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
